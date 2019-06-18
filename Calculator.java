4fa0449c61d9981dac82dfb5d83dc453bf21eefe
TalkTomeG00se <47369415+TalkTomeG00se@users.noreply.github.com>
2019-06-18 12:43:12 -0600

    Update to Caculator.java
    Implemented basic skeleton for program

--- Calculator.java
+++ Calculator.java
@@ -1,1 +1,26 @@
 // Java Calculator
+
+import java.util.Scanner;
+
+public class Calculator {
+
+	public static void main(String[] args) {
+		
+		double answer; // Storing the eventual answer as a double, change as needed.
+		
+		Scanner scanner = new Scanner(System.in); // New scanner for user input
+		
+		System.out.print("Enter A Mathematical Expression: "); // Prompt user for input
+		
+		// TO DO: Code to validate/parse/store user input
+		
+		scanner.close(); // Closing scanner. Move around as necessary.
+		
+		// TO DO: Code to perform math functions w/ user input
+		
+		System.out.print("Answer: " + answer); // Print out answer
+		
+
+	}
+
+}
