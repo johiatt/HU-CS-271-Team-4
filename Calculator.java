4baa43a11b53b2f1821b9438d6bd632176ed5b19
kirmal-mal <kirmal.mal@gmail.com>
2019-06-23 01:26:29 -0600

    [Issue #9] Added starting functionaliti for command line with some TODOs

--- Calculator.java
+++ Calculator.java
@@ -5,20 +5,64 @@ import java.util.Scanner;
 public class Calculator {
 
 	public static void main(String[] args) {
-		
-		double answer; // Storing the eventual answer as a double, change as needed.
-		
+
+		double answer = 0; // Storing the eventual answer as a double, change as needed.
+		boolean previous = false; //True, if we use previous result as a first argument of expressions
+
 		Scanner scanner = new Scanner(System.in); // New scanner for user input
-		
-		System.out.print("Enter A Mathematical Expression: "); // Prompt user for input
-		
-		// TO DO: Code to validate/parse/store user input
-		
-		scanner.close(); // Closing scanner. Move around as necessary.
-		
-		// TO DO: Code to perform math functions w/ user input
-		
-		System.out.print("Answer: " + answer); // Print out answer
+
+		if (args.length == 0) {
+			String input;
+			do {
+
+				System.out.print("Enter A Mathematical Expression (\"C\" to erase answer. supported \"+\",\"-\",\"*\",\"/\"): "); // Prompt user for input
+				input = scanner.next();
+				String operation = input;//TODO: Add mathematical expression input check
+
+				double argument1, argument2;
+
+
+				switch (operation) {
+					case "+": {
+						if (previous) {
+							argument1 = answer;
+						} else {
+							System.out.print("Enter First Argument: ");
+							input = scanner.next();
+							argument1 = Double.parseDouble(input); // TODO: replace with input check for arguments
+						}
+						System.out.print("Enter Second Argument: ");
+						input = scanner.next();
+						argument2 = Double.parseDouble(input); // TODO: replace with input check for arguments
+						answer = add(argument1, argument2);
+						System.out.println("Answer: " + answer); // Print out answer
+						previous = true;
+						break;
+					}
+					case "C":{}
+					case "c":{
+						previous = false;
+						break;
+					}
+					default: {
+						System.out.println(input + " is unsupported operation");
+					}
+				}
+
+			} while (!input.equals(""));
+
+			scanner.close(); // Closing scanner. Move around as necessary.
+
+		} else {
+			System.out.println("Expression evaluation is not supported!");
+			System.exit(0); // Delete this string if expressions are supported
+			if (args.length == 1) {
+				String expression = args[0];
+				//TODO: Evaluation of expressions
+			} else {
+				//TODO: Create function that prints "how to use"
+			}
+		}
 		
 
 	}
@@ -28,8 +72,8 @@ public class Calculator {
 	private static double divide (int a, int b){
 		return (a/b);
 	}
-	private static double subtract (double a, double b) {
-		return (a-b);
+	private static double add (double a, double b) {
+		return a+b;
 	}
 
 }
